71b149600e61cea06be74fa4e5390ff5108b19ee
Elaidzha1940 <eli080293@gmail.com>
2023-10-18 21:46:37 +0300

    LoadingAniamtion

---
+++ LoadingAniamtion/LoadingAniamtionApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: LoadingAniamtion
+//  File: LoadingAniamtionApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 18.10.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct LoadingAniamtionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
