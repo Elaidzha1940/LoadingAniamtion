fa6744d8bde57b0aca47e05e5c8119836d0e5f2d
Elaidzha1940 <eli080293@gmail.com>
2023-10-19 18:27:54 +0300

    Added Text and Updated .mask

--- LoadingAniamtion/ContentView.swift
+++ LoadingAniamtion/ContentView.swift
@@ -10,10 +10,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var start = false
+    
     var body: some View {
         
-        VStack {
+        ZStack {
             
+            Text("Loading")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+            
+            Text("Loading")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .foregroundStyle(.black)
+                .frame(width: 200, height: 100)
+                .background(Color.black.opacity(0.9))
+                .mask {
+                    Circle()
+                        .frame(width: 50, height: 50)
+                        .offset(x: start ? -50 : 50)
+
+                }
         }
         .preferredColorScheme(.dark)
     }
