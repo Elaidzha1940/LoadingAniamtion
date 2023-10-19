41e0c50bcf871e885cb4ab3a415ef4a82eb048d2
Elaidzha1940 <eli080293@gmail.com>
2023-10-19 18:44:01 +0300

    Fixed some Bugs

--- LoadingAniamtion/ContentView.swift
+++ LoadingAniamtion/ContentView.swift
@@ -16,34 +16,34 @@ struct ContentView: View {
         
         ZStack {
             
-            Text("Loading")
+            Text("Dowloading")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
             
-            Text("Loading")
+            Text("Dowloading")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .foregroundStyle(.black)
                 .frame(width: 200, height: 100)
-                .background(Color.black.opacity(0.9))
+                .background(Color.white.opacity(0.7))
                 .mask {
                     Circle()
                         .frame(width: 50, height: 50)
-                        .offset(x: start ? -50 : 50)
+                        .offset(x: start ? -70 : 70)
                 }
             
             Circle()
                 .stroke(lineWidth: 3)
                 .frame(width: 50, height: 50)
-                .offset(x: start ? -50 : 50)
+                .offset(x: start ? -70 : 70)
         }
         .frame(width: 250, height: 150)
         .preferredColorScheme(.dark)
         .background(
             Color.black,
             in: RoundedRectangle(cornerRadius: 10))
-        .shadow(color: .black.opacity(0.1), radius: 10)
+        .shadow(color: .clear.opacity(0.1), radius: 10)
         .onAppear {
-            withAnimation(.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
-                <#code#>
+            withAnimation(.easeInOut(duration: 1.5).repeatForever(autoreverses: true)) {
+                start = true
             }
         }
     }
